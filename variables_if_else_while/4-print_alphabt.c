8305dae059e3afd91467c19f3c01bdf6ae2b4208
FreakJLH2025 <joshua.luja@gmail.com>
2025-10-15 15:31:34 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letters[] = "abcdfghijklmnoprstuvwxyz\n";
+int i = 0;
+
+while (letters[i])
+{
+putchar(letters[i]);
+i++;
+}
+
+return (0);
+}
