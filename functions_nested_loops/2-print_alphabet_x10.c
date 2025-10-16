8bfc4e1988c2d5b6ef2b96a8ace681e0a95ea019
FreakJLH2025 <joshua.luja@gmail.com>
2025-10-16 17:21:51 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the alphabet 10 times in lowercase
+*/
+void print_alphabet_x10(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+j = 0;
+while (alphabet[j] != '\0')
+{
+_putchar(alphabet[j]);
+j++;
+}
+}
+}
