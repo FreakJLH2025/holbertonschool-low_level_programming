25f53c529a8ce22f118554ba143d3c83532a1eac
FreakJLH2025 <joshua.luja@gmail.com>
2025-10-21 17:33:56 +0000

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints numbers from 0 to 9, except 2 and 4
+*
+* Return: void
+*/
+void print_most_numbers(void)
+{
+char nums[] = "01356789\n";
+int i = 0;
+
+while (nums[i] != '\0')
+{
+_putchar(nums[i]);
+i++;
+}
+}
