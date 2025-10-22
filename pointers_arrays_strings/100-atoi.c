acf655b5cfc281d35697d5a374ab59b806ba3435
FreakJLH2025 <joshua.luja@gmail.com>
2025-10-22 21:38:00 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,23 +12,30 @@ int _atoi(char *s)
 int i = 0;
 int sign = 1;
 int result = 0;
-int found_digit = 0;
+int started = 0;
 
 while (s[i] != '\0')
 {
 if (s[i] == '-')
+{
 sign *= -1;
-else if (s[i] == '+')
-; /* do nothing */
+}
 else if (s[i] >= '0' && s[i] <= '9')
 {
-found_digit = 1;
+started = 1;
 result = result * 10 + (s[i] - '0');
 }
-else if (found_digit)
+else if (started)
+{
 break;
+}
 i++;
 }
 
-return (result * sign);
+if (sign == -1)
+{
+result = -result;
+}
+
+return result;
 }
