7851c648938504b477254206916d1c2638f8d68e
FreakJLH2025 <joshua.luja@gmail.com>
2025-10-22 16:19:26 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*
+*main - reset and updates the value to 98
+*
+*Return always to 0 
+*
+*/
+int main() 
+{
+int value = 10;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
+}
