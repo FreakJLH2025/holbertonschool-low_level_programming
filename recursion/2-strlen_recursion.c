acb24d086f387c48ada810a5b8f560b14b919bb8
root <joshua.luja@gmail.com>
2025-10-28 18:59:39 +0000

    2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen_recursion - Returns the length of a string
+* @s: Pointer to the string
+*
+* This function recursively counts characters until it reaches
+* the null terminator '\0', returning the total length.
+*/
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+
+return (1 + _strlen_recursion(s + 1));
+}
