0ad07ce36e49c8f0146c65da622b7fb3f75fb20d
root <joshua.luja@gmail.com>
2025-10-28 18:34:37 +0000

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,17 +5,18 @@
 * _puts_recursion - Prints a string followed by a new line
 * @s: Pointer to the string to be printed
 *
-* This function uses recursion to print each character of the string
-* one by one, followed by a newline when the null terminator is reached.
+* This function uses recursion to print each character of the string.
+* It avoids using putchar, relying instead on printf (allowed) or puts.
 */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')  /* Base case: end of string */
+if (*s == '\0')
 {
-putchar('\n');
+puts("");  /* Print newline only */
 return;
 }
 
-putchar(*s);     /* Print current character */
-_puts_recursion(s + 1);  /* Recurse with next character */
+printf("%c", *s);         /* Print current character */
+_puts_recursion(s + 1);   /* Recurse with next character */
 }
+
