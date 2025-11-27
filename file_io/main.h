3ce696767ffca4ca1d43f1922cd267e72a8e17c3
FreakJLH2025 <joshua.luja@gmail.com>
2025-11-27 18:58:57 +0000

    main.h

---
+++ file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif 
