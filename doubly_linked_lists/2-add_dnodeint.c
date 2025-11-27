694f50842f214035f1e53fb4f750ec4ab6ac9378
FreakJLH2025 <joshua.luja@gmail.com>
2025-11-27 15:43:03 +0000

    2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,12 +9,15 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new_node = malloc(sizeof(dlistint_t));
+dlistint_t *new_node;
 
+new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 
-*new_node = (dlistint_t){n, NULL, *head};
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *head;
 
 if (*head != NULL)
 (*head)->prev = new_node;
