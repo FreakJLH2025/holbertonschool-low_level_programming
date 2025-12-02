1afc266eb54ca067e023d8e8f4c432fb345f8afa
FreakJLH2025 <joshua.luja@gmail.com>
2025-12-02 16:45:45 +0000

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -19,17 +19,40 @@ unsigned long int idx;
 hash_node_t *node, *tmp;
 char *val_copy;
 
-if (!ht || !key || !*key || !value) return (0);
+if (!ht || !key || !*key || !value)
+return (0);
 idx = key_index((const unsigned char *)key, ht->size);
-for (tmp = ht->array[idx]; tmp; tmp = tmp->next)
+tmp = ht->array[idx];
+while (tmp)
+{
 if (strcmp(tmp->key, key) == 0)
-{ val_copy = strdup(value); if (!val_copy) return (0);
-free(tmp->value); tmp->value = val_copy; return (1); }
+{
+val_copy = strdup(value);
+if (!val_copy)
+return (0);
+free(tmp->value);
+tmp->value = val_copy;
+return (1);
+}
+tmp = tmp->next;
+}
 node = malloc(sizeof(hash_node_t));
-if (!node) return (0);
-node->key = strdup(key); node->value = strdup(value);
-if (!node->key || !node->value)
-{ free(node->key); free(node->value); free(node); return (0); }
-node->next = ht->array[idx]; ht->array[idx] = node;
+if (!node)
+return (0);
+node->key = strdup(key);
+if (!node->key)
+{
+free(node);
+return (0);
+}
+node->value = strdup(value);
+if (!node->value)
+{
+free(node->key);
+free(node);
+return (0);
+}
+node->next = ht->array[idx];
+ht->array[idx] = node;
 return (1);
 }
